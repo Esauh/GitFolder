73da7cca6892a60cafd4454fd621b6c0023a2231
Esauh <esau.hutcherson@bison.howard.edu>
2021-04-29 16:10:26 +0000

    My project commit

--- caesarcipher.cpp
+++ caesarcipher.cpp
@@ -3,61 +3,59 @@
 #include<algorithm>
 using namespace std;
 
-string CaesarCipher(int,string);
 
 int main() {
-  int number;
-  cout << "Type amount of times you want letters be be shifting by number" << endl;
-  cin >> number;
- string input = "";
- cout << "Type message to be encrypted without adding spaces" << endl;
- cin >> input;
-input = CaesarCipher(number, input);
-cout << input;
-}
+string input;
+ int shift, length, count;
+ cout << "Type message to be encrypted" << endl;
+ getline(cin, input);
+ cout << "Type encryption shift amount" << endl;
+cin >> shift;
 
-string CaesarCipher(int shift, string text)
+length = input.length();
+
+for(count = 0; count < length; count++)
 {
-string result = " ";
-remove(text.begin(), text.end(), ' ');
-
-
-
-for(int i = 0; i < text.length(); i++)
-{
-result += toupper(text[i]);
-
+    if (isalpha(input[count]))
+    {
+        input[count] = toupper(input[count]);
+        for (int i = 0; i < shift; i++)
+        {
+            if(input[count] == 'Z')
+              input[count] = 'A';
+              else 
+              input[count]++;
+        }
+    }
 }
 
-string result2 = "";
-for (int i =0; i < result.length(); i++)
-{      
-  result2 += char(int(result[i]+shift-65)%26 +65);
-}
-
-for (int i = 0; i < result2.size(); i++) {       
-  if ((result2[i] < 'A' || result2[i] > 'Z') && (result2[i]< 'a' || result2[i] > 'z'))
+for (int i = 0; i < input.size(); i++) {       
+  if ((input[i] < 'A' || input[i] > 'Z') && (input[i]< 'a' || input[i] > 'z'))
 
  {   
-result2.erase(i, 1); 
+input.erase(i, 1); 
  i--;
 }
 }
 
-string output = " ";
+
+string output;
+int count2 = 0;
+
+  for( int i =0; i < input.length(); i= i+5)
+  {
+  output += input.substr(i, 5) + " "; 
+  count2++;
+    if(count2 == 10)
+    {
+     output += "\n";
+     count2 = count2-10;
+    }
+  }
 
 
-for( int i =0; i < result2.length(); i= i+5)
-{
-output += result2.substr(i, 5) + " "; 
+
+
+cout << output;
 
 }
-
-
-
-return output;
-
-
-
-}
-
